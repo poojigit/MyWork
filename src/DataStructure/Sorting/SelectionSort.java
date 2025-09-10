3b838d900e6ff927afb4aa239586ba377af55535
PoojiGit <lakshyapoojisha@gmail.com>
2025-09-10 13:06:40 +0530

    All Sorting Codes

--- src/DataStructure/Sorting/SelectionSort.java
+++ src/DataStructure/Sorting/SelectionSort.java
@@ -1,4 +1,38 @@
 package DataStructure.Sorting;
 
+import java.util.Scanner;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of array: ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<n-1; i++){
+            int minIndex=i;
+            for(int j=i+1; j<n; j++){
+                if (arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+
+            if(minIndex!=i){
+                int temp=arr[i];
+                arr[i]=arr[minIndex];
+                arr[minIndex]=temp;
+            }
+        }
+
+        System.out.println("Sorted Array is: ");
+        for(int x:arr){
+            System.out.print(x + " ");
+        }
+    }
 }
